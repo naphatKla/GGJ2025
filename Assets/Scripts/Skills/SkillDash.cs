ca6af46b7f8b318b33cac4c138a23e0255271aaf
Kla <naphat.watt@bumail.net>
2025-01-24 20:44:14 +0700

    Lost exp

--- Assets/Scripts/Skills/SkillDash.cs
+++ Assets/Scripts/Skills/SkillDash.cs
@@ -10,6 +10,8 @@ namespace Skills
         [SerializeField] [ValidateInput("@backStepDuration <= skillDuration")] private float backStepDuration = 0.25f;
         [SerializeField] private float minDashForce = 5f;
         [SerializeField] private float maxDashForce = 10f;
+        [SerializeField] [Unit(Units.Percent)] private float minLostOxygen;
+        [SerializeField] [Unit(Units.Percent)] private float maxLostOxygen;
         
         private void Start()
         {
@@ -23,18 +25,21 @@ namespace Skills
 
         protected override void SkillAction()
         {
-            float dashForce = maxDashForce * chargePercentage;
+            float dashForce = Mathf.Lerp(minDashForce, maxDashForce, chargePercentage);
+            float lostOxygen = Mathf.Lerp(minLostOxygen, maxLostOxygen, chargePercentage);
             Vector2 direction = new Vector2();
+            
             if (OwnerCharacter.CompareTag("Player")) 
                 direction = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - OwnerCharacter.transform.position).normalized;
             else 
                 direction = OwnerCharacter.rigidbody2D.velocity.normalized;
             
+            OwnerCharacter.AdjustSize(-lostOxygen);
             OwnerCharacter.rigidbody2D.velocity = Vector2.zero;
             OwnerCharacter.rigidbody2D.AddForce(-direction * backStepForce);
             DOVirtual.DelayedCall(backStepDuration, () =>
             {
-                OwnerCharacter.rigidbody2D.AddForce(direction * (maxDashForce));
+                OwnerCharacter.rigidbody2D.AddForce(direction * dashForce);
             });
         }
     }
