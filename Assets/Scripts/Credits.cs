d64d0757056b3c0c1d7e25a4d01fddfe6c79b793
ShaoKuy <iqtanawad@gmail.com>
2025-01-24 16:42:21 +0700

    scene credits V.1

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement; // หากใช้ TextMeshPro
 
 public class Credits : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public RectTransform creditsText;
+    public float scrollSpeed = 50f; 
+    public float endPositionY = 1000f;
+    private Vector3 startPosition;
+
     void Start()
     {
-        
+        startPosition = creditsText.localPosition; 
     }
 
-    // Update is called once per frame
     void Update()
     {
+        creditsText.localPosition += Vector3.up * scrollSpeed * Time.deltaTime;
         
+        if (creditsText.localPosition.y >= endPositionY)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
     }
 }
