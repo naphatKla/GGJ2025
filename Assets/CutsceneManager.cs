7f92563c4eb6706cc25647b4733ce120e5e8130c
facekak <facekak5060@gmail.com>
2025-02-11 15:16:36 +0700

    Audio and function to play

--- Assets/CutsceneManager.cs
+++ Assets/CutsceneManager.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class CutsceneManager : MonoBehaviour
 {
+    public PlayableDirector playableDirector;
+
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
+    }
+
+    [Button("Play Cutscene")]
+    public void PlayCutscene()
+    {
+        playableDirector.gameObject.SetActive(true);
+        playableDirector.Play();
     }
 
     public void FreezeTheGame()
