279f3d6e81299ae0922066cb2340ba8f8fccf730
Kla <naphat.watt@bumail.net>
2025-05-04 18:24:31 +0700

    SAVE Point

--- Assets/Scripts/Characters/SO/SkillDataSo/BaseSkillDataSo.cs
+++ Assets/Scripts/Characters/SO/SkillDataSo/BaseSkillDataSo.cs
@@ -1,21 +1,31 @@
+using System;
+using Characters.SkillSystems.SkillRuntimes;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Characters.SO.SkillDataSo
 {
     /// <summary>
-    /// Abstract base class for all skill data ScriptableObjects.
-    /// Defines common configuration values shared by all skills, such as cooldown duration.
-    /// Inherit from this class to create custom data containers for specific skill types.
+    /// 
     /// </summary>
     public abstract class BaseSkillDataSo : ScriptableObject
     {
         [PropertyTooltip("The cooldown duration (in seconds) before the skill can be used again.")]
         [SerializeField] private float cooldown = 1f;
+        
+        [Title("")] [ShowInInspector] [PropertyOrder(10000)]
+        [PropertyTooltip("")]
+        [TypeDrawerSettings(BaseType = typeof(BaseSkillRuntime))]
+        private Type _skillRuntime;
 
         /// <summary>
         /// Gets the cooldown duration (in seconds) for this skill.
         /// </summary>
         public float Cooldown => cooldown;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Type SkillRuntime => _skillRuntime;
     }
 }
