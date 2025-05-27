b5b47ed5aedc5b8a7396726ef3c91b332504800e
auttasit23 <auttasit.umpo@bumail.net>
2025-05-27 14:01:00 +0700

    Remove not use

---
+++ Assets/Samples/02_FocusOn/Cell.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * FancyScrollView (https://github.com/setchi/FancyScrollView)
+ * Copyright (c) 2020 setchi
+ * Licensed under MIT (https://github.com/setchi/FancyScrollView/blob/master/LICENSE)
+ */
+
+namespace UnityEngine.UI.Extensions.Examples.FancyScrollViewExample02
+{
+    class Cell : FancyCell<ItemData, Context>
+    {
+        [SerializeField] Animator animator = default;
+        [SerializeField] Text message = default;
+        [SerializeField] Image image = default;
+        [SerializeField] Button button = default;
+
+        static class AnimatorHash
+        {
+            public static readonly int Scroll = Animator.StringToHash("scroll");
+        }
+
+        public override void Initialize()
+        {
+            button.onClick.AddListener(() => Context.OnCellClicked?.Invoke(Index));
+        }
+
+        public override void UpdateContent(ItemData itemData)
+        {
+            var selected = Context.SelectedIndex == Index;
+            image.color = selected
+                ? new Color32(210, 210, 210, 255) //Select
+                : new Color32(255, 255, 255, 50); //Not Select
+        }
+
+        public override void UpdatePosition(float position)
+        {
+            currentPosition = position;
+
+            if (animator.isActiveAndEnabled)
+            {
+                animator.Play(AnimatorHash.Scroll, -1, position);
+            }
+
+            animator.speed = 0;
+        }
+        
+        float currentPosition = 0;
+
+        void OnEnable() => UpdatePosition(currentPosition);
+    }
+}
