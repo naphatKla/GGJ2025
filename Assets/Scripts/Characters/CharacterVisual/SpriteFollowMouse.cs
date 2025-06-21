29b8c637dcfcc4361b0072b8d4e7651b3e208b78
Kla <naphat.watt@bumail.net>
2025-06-21 16:20:07 +0700

    save

--- Assets/Scripts/Characters/CharacterVisual/SpriteFollowMouse.cs
+++ Assets/Scripts/Characters/CharacterVisual/SpriteFollowMouse.cs
@@ -9,6 +9,7 @@ public class SpriteFollowMouse : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (ownerMovementSystem.CurrentVelocity.magnitude < 0.001f) return;
         _sprite.transform.up = ownerMovementSystem.CurrentVelocity.normalized;
     }
 }
