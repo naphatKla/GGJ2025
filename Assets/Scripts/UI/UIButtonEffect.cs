40e902afda395c90897637074d1209e206266131
ShaoKuy <iqtanawad@gmail.com>
2025-06-24 11:22:48 +0700

    Save Game UI

---
+++ Assets/Scripts/UI/UIButtonEffect.cs
@@ -0,0 +1,89 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.EventSystems;
+
+public class UIButtonEffect : MonoBehaviour
+{
+    [Header("Assign Buttons")]
+    [SerializeField] private List<Button> buttonsWithEffect;
+
+    [Header("Hover & Click Settings")]
+    [SerializeField] private float hoverScale = 1.05f;
+    [SerializeField] private float pressScale = 1.15f;
+    [SerializeField] private float tweenDuration = 0.2f;
+    [SerializeField] private Ease easeType = Ease.OutBack;
+
+    [Header("Alpha Settings")]
+    [SerializeField] private float normalAlpha = 0f;
+    [SerializeField] private float hoverAlpha = 1f;
+
+    void Start()
+    {
+        foreach (Button button in buttonsWithEffect)
+        {
+            AddEffectToButton(button);
+        }
+    }
+
+    void AddEffectToButton(Button button)
+    {
+        RectTransform rectTransform = button.GetComponent<RectTransform>();
+        Vector3 originalScale = rectTransform.localScale;
+
+        Image backgroundImage = button.GetComponent<Image>();
+        if (backgroundImage != null)
+        {
+            Color c = backgroundImage.color;
+            c.a = normalAlpha;
+            backgroundImage.color = c;
+        }
+
+        EventTrigger trigger = button.gameObject.GetComponent<EventTrigger>();
+        if (trigger == null) trigger = button.gameObject.AddComponent<EventTrigger>();
+
+        AddTrigger(trigger, EventTriggerType.PointerEnter, (data) =>
+        {
+            rectTransform.DOKill();
+            rectTransform.DOScale(originalScale * hoverScale, tweenDuration).SetEase(easeType);
+
+            if (backgroundImage != null)
+            {
+                backgroundImage.DOFade(hoverAlpha, tweenDuration);
+            }
+        });
+
+        AddTrigger(trigger, EventTriggerType.PointerExit, (data) =>
+        {
+            rectTransform.DOKill();
+            rectTransform.DOScale(originalScale, tweenDuration).SetEase(Ease.OutQuad);
+
+            if (backgroundImage != null)
+            {
+                backgroundImage.DOFade(normalAlpha, tweenDuration);
+            }
+        });
+
+        AddTrigger(trigger, EventTriggerType.PointerDown, (data) =>
+        {
+            rectTransform.DOKill();
+            rectTransform.DOScale(originalScale * pressScale, tweenDuration).SetEase(easeType);
+        });
+
+        AddTrigger(trigger, EventTriggerType.PointerUp, (data) =>
+        {
+            rectTransform.DOKill();
+            bool isHover = ((PointerEventData)data).pointerEnter == button.gameObject;
+            float targetScale = isHover ? hoverScale : 1f;
+            rectTransform.DOScale(originalScale * targetScale, tweenDuration).SetEase(Ease.OutBack);
+        });
+    }
+
+    void AddTrigger(EventTrigger trigger, EventTriggerType eventType, UnityEngine.Events.UnityAction<BaseEventData> action)
+    {
+        EventTrigger.Entry entry = new EventTrigger.Entry { eventID = eventType };
+        entry.callback.AddListener(action);
+        trigger.triggers.Add(entry);
+    }
+}
