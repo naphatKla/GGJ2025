85c16591bf3ade98076fa2e237050ba2144012ac
Kla <naphat.watt@bumail.net>
2025-06-27 07:39:32 +0700

    Mockup variable

--- Assets/Scripts/Characters/SkillSystems/EnemySkillSystem.cs
+++ Assets/Scripts/Characters/SkillSystems/EnemySkillSystem.cs
@@ -1,21 +1,24 @@
 using Characters.FeedbackSystems;
 using Cysharp.Threading.Tasks;
+using UnityEngine;
 
 namespace Characters.SkillSystems
 {
     public class EnemySkillSystem : SkillSystem
     {
+        [SerializeField] private float chargeMockupDelay = 0.5f;
+        
         // TODO: Delete this and implement system, this is for mockup test
         private bool _isCharging;
         public override async void PerformPrimarySkill()
         {
             if (!_owner) return;
             if (_isCharging) return;
-            if (_primarySkillCooldown > 0.5f) return;
+            if (_primarySkillCooldown > chargeMockupDelay) return;
 
             _isCharging = true;
             _owner.FeedbackSystem.PlayFeedback(FeedbackName.Charge);
-            await UniTask.Delay(500);
+            await UniTask.WaitForSeconds(chargeMockupDelay);
             base.PerformPrimarySkill();
             _isCharging = false;
         }
