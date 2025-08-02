2a35c65c6fcb52b63bc26fdec839104cceeddc86
auttasit.umpo <auttasit.umpo@gmail.com>
2025-08-02 18:23:07 +0700

    MapSelectionMenu

---
+++ Assets/Scripts/UI/MapSelection/MapSelectionPresenter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI.Extensions.Examples.FancyScrollViewExample03;
+
+namespace UI.MapSelection
+{
+    class MapSelectionPresenter : MonoBehaviour
+    {
+        [SerializeField] MapSelectionScrollView mapSelectionScrollView;
+        [SerializeField] public MapSelectionDataContainer mapSelectionDataContainer;
+        
+
+        void Start()
+        {
+            var items = new List<MapSelectionItemModel>();
+            foreach (var mapData in mapSelectionDataContainer.mapSelectionList)
+            {
+                items.Add(new MapSelectionItemModel($"{mapData.mapName}", mapData.image));
+            }
+            
+            mapSelectionScrollView.UpdateData(items);
+            mapSelectionScrollView.SelectCell(0);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/MapSelection/MapSelectionScrollView.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * FancyScrollView (https://github.com/setchi/FancyScrollView)
+ * Copyright (c) 2020 setchi
+ * Licensed under MIT (https://github.com/setchi/FancyScrollView/blob/master/LICENSE)
+ */
+
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI.Extensions;
+using UnityEngine.UI.Extensions.EasingCore;
+
+namespace UI.MapSelection
+{
+    class MapSelectionScrollView : FancyScrollView<MapSelectionItemModel, MapSelectionContext>
+    {
+        [SerializeField] Scroller scroller = default;
+        [SerializeField] GameObject cellPrefab = default;
+
+        protected override GameObject CellPrefab => cellPrefab;
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            Context.OnCellClicked = SelectCell;
+
+            scroller.OnValueChanged(UpdatePosition);
+            scroller.OnSelectionChanged(UpdateSelection);
+        }
+
+        void UpdateSelection(int index)
+        {
+            if (Context.SelectedIndex == index)
+            {
+                return;
+            }
+
+            Context.SelectedIndex = index;
+            MapSelectionSender.Instance.currentMapSelectionIndex = index;
+            Refresh();
+        }
+
+        public void UpdateData(IList<MapSelectionItemModel> items)
+        {
+            UpdateContents(items);
+            scroller.SetTotalCount(items.Count);
+        }
+
+        public void SelectCell(int index)
+        {
+            if (index < 0 || index >= ItemsSource.Count || index == Context.SelectedIndex)
+            {
+                return;
+            }
+
+            UpdateSelection(index);
+            scroller.ScrollTo(index, 0.35f, Ease.OutCubic);
+        }
+    }
+}
