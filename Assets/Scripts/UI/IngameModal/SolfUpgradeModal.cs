0457a694d4a5f1f09f2114b90e056fa8dfdea3f3
auttasit.umpo <auttasit.umpo@gmail.com>
2025-08-06 17:39:49 +0700

    SolfUpgradePanelPopup

---
+++ Assets/Scripts/UI/IngameModal/SolfUpgradeModal.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Characters.SO.SkillDataSo;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.IngameModal
+{
+    public class SolfUpgradeModal : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text skillTitle;
+        [SerializeField] private Image skillIcon;
+        [SerializeField] private TMP_Text skillDescription;
+        [SerializeField] private Button selectButton;
+
+        public Button SelectButton => selectButton;
+
+        public void UpdateUIModal(BaseSkillDataSo data)
+        {
+            skillTitle.text = data.name;
+            skillIcon.sprite = data.SkillIcon;
+            skillDescription.text = data.SkillDescription;
+        }
+    }
+}
