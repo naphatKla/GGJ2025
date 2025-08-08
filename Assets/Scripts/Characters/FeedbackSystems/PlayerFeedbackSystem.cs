8602b5449e54df33b38cde383bf7fff8a2a33262
Kla <naphat.watt@bumail.net>
2025-08-08 19:56:59 +0700

    Save

--- Assets/Scripts/Characters/FeedbackSystems/PlayerFeedbackSystem.cs
+++ Assets/Scripts/Characters/FeedbackSystems/PlayerFeedbackSystem.cs
@@ -1,13 +1,21 @@
+using Cameras;
 using UnityEngine;
 
 namespace Characters.FeedbackSystems
 {
-    public class PlayerFeedback : FeedbackSystem
+    public class PlayerFeedbackSystem : FeedbackSystem
     {
+        [SerializeField] private Cinemachine2DCameraController _cameraController;
+        
         public override void PlayFeedback(FeedbackName feedbackName)
         {
             if (!feedbackList.ContainsKey(feedbackName)) return;
             if (ignoreFeedbackList.Contains(feedbackName)) return;
+            
+            if (feedbackName == FeedbackName.AttackHit)
+            {
+                
+            }
             
             base.PlayFeedback(feedbackName);
         }
