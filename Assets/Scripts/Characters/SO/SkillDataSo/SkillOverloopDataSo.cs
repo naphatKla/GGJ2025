1b6e2254b8b524e5e007dbb7f499ae7d15bab8aa
Kla <naphat.watt@bumail.net>
2025-08-10 15:10:50 +0700

    Save

--- Assets/Scripts/Characters/SO/SkillDataSo/SkillOverloopDataSo.cs
+++ Assets/Scripts/Characters/SO/SkillDataSo/SkillOverloopDataSo.cs
@@ -5,6 +5,8 @@ namespace Characters.SO.SkillDataSo
     [CreateAssetMenu(fileName = "SkillOverloopData", menuName = "GameData/SkillData/SkillOverloopData")]
     public class SkillOverloopDataSo : BaseSkillDataSo
     {
-     
+        [SerializeField] private int amountSkillToReset = 1;
+        [SerializeField] private int resetRound = 1;
+        [SerializeField] private float delayPerRound = 1f;
     }
 }
