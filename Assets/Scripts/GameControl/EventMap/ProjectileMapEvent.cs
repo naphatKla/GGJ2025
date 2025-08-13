cbd9dd136399bc233a026796f9785cb73ec8b220
auttasit.umpo <auttasit.umpo@gmail.com>
2025-08-13 17:46:01 +0700

    Save

--- Assets/Scripts/GameControl/EventMap/ProjectileMapEvent.cs
+++ Assets/Scripts/GameControl/EventMap/ProjectileMapEvent.cs
@@ -1,3 +1,4 @@
+using System;
 using Cysharp.Threading.Tasks;
 using Manager;
 using UnityEngine;
@@ -11,12 +12,17 @@ namespace GameControl.EventMap
         
         [Header("Hitbox Settings")]
         [SerializeField] private Vector3 hitboxSize = new Vector3(2f, 80f, 0);
-        [SerializeField] private Vector3 hitboxOffset = new Vector3(-1f, 0f, 0);
+        [SerializeField] private Vector3 hitboxOffset = new Vector3(0, 0f, 0);
         [SerializeField] private LayerMask hitLayer;
 
         public HitboxType HitboxType => HitboxType.Box;
         public Vector3 Size { get => hitboxSize; set => hitboxSize = value; }
         public Vector3 Offset { get => hitboxOffset; set => hitboxOffset = value; }
+
+        private void OnValidate()
+        {
+            ApplySizeToEffect();
+        }
 
         private void ApplySizeToEffect()
         {
@@ -37,22 +43,23 @@ namespace GameControl.EventMap
 
         protected override void Perform()
         {
-            Vector3 center = firePoint.TransformPoint(hitboxOffset + hitboxSize/2 );
+            Vector3 center = firePoint.TransformPoint(hitboxOffset + hitboxSize/2);
 
             Collider[] hits = Physics.OverlapBox(center, hitboxSize * 0.5f, firePoint.rotation, hitLayer);
 
-            foreach (var hit in hits) 
-                CombatManager.ApplyRawDamageTo(hit.gameObject, damage);
+            foreach (var hit in hits) CombatManager.ApplyRawDamageTo(hit.gameObject, damage);
         }
         
         private void OnDrawGizmos()
         {
             if (firePoint == null) return;
 
-            Gizmos.color = new Color(1f, 0f, 0f, 0.5f);
+            Gizmos.color = new Color(1f, 0f, 0f, 0.2f);
 
+            Vector3 center = firePoint.TransformPoint(hitboxOffset + hitboxSize/2);
+            
             Matrix4x4 rotationMatrix = Matrix4x4.TRS(
-                firePoint.TransformPoint(hitboxOffset + hitboxSize/2 ),
+                center,
                 firePoint.rotation,
                 Vector3.one
             );
