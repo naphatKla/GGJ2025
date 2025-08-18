047902cea80379f191694600a0fd1a4251138f98
Kla <naphat.watt@bumail.net>
2025-08-18 17:48:09 +0700

    Save

--- Assets/Scripts/Manager/PoolingManager.cs
+++ Assets/Scripts/Manager/PoolingManager.cs
@@ -14,6 +14,7 @@ namespace Manager
         public static string Item => nameof(Item);
 
         public static string UI => nameof(UI);
+        public static string Sound => nameof(Sound);
     }
 
     public class PoolingManager : MMSingleton<PoolingManager>
