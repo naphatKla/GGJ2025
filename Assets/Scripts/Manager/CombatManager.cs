56155634f8c7b914b62e622ddc7ef35d1b891b1a
Kla <naphat.watt@bumail.net>
2025-09-06 00:55:10 +0700

    Real 0.0.3

--- Assets/Scripts/Manager/CombatManager.cs
+++ Assets/Scripts/Manager/CombatManager.cs
@@ -72,7 +72,7 @@ namespace Manager
             attackerController.CombatSystem.OnDealDamageHandler(damageData);
 
             if (targetController.FeedbackSystem is PlayerFeedbackSystem playerFeedback)
-                playerFeedback.OpenFocusBlackDropOnHit(0.525f, attacker);
+                playerFeedback.OpenFocusBlackDropOnHit(0.4f, attacker);
             
             if (dieThisFrame)
                 attackerController.CombatSystem.OnKillHandler();
@@ -91,7 +91,7 @@ namespace Manager
             if (!targetController.HealthSystem.TakeDamage(damage, out _)) return;
             
             if (targetController.FeedbackSystem is PlayerFeedbackSystem playerFeedback)
-                playerFeedback.OpenFocusBlackDropOnHit(0.525f, attacker);
+                playerFeedback.OpenFocusBlackDropOnHit(0.4f, attacker);
         }
 
         public static void ClearCache()
