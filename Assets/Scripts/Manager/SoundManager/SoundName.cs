df633094cb62a05d4f677158082d5f307b8dc5f9
Kla <naphat.watt@bumail.net>
2025-09-05 20:45:52 +0700

    Warning text & sound

--- Assets/Scripts/Manager/SoundManager/SoundName.cs
+++ Assets/Scripts/Manager/SoundManager/SoundName.cs
@@ -57,6 +57,7 @@ namespace Manager.SoundManager
             public const string Reward       = Group + "/" + nameof(Reward);
             public const string Error        = Group + "/" + nameof(Error);
             public const string CountDown5Sec        = Group + "/" + nameof(CountDown5Sec);
+            public const string WarningAlert = Group + "/" + nameof(WarningAlert);
         }
 
         public static class BGM
