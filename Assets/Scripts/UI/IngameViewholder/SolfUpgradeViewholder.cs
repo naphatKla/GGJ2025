7e22d3791f6dc7936c9f83286eb7c83d288d63fe
auttasit.umpo <auttasit.umpo@gmail.com>
2025-09-07 18:52:21 +0700

    SolfSkillSelectButton

--- Assets/Scripts/UI/IngameViewholder/SolfUpgradeViewholder.cs
+++ Assets/Scripts/UI/IngameViewholder/SolfUpgradeViewholder.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Characters.SO.SkillDataSo;
@@ -13,10 +14,39 @@ namespace UI.IngameViewholder
         [SerializeField] private TMP_Text skillTitle;
         [SerializeField] private Image skillIcon;
         [SerializeField] private TMP_Text skillDescription;
-        [SerializeField] private Button selectButton;
+        [SerializeField] private GameObject selectEffect;
         [SerializeField] private TMP_Text newSkillText;
+        [SerializeField] private Button raycast;
 
-        public Button SelectButton => selectButton;
+        public BaseSkillDataSo Data { get; private set; }
+        public bool IsSelected { get; private set; }
+
+        public event Action<SolfUpgradeViewholder> Clicked;
+
+        public void Bind(BaseSkillDataSo data, bool isNew, Action<SolfUpgradeViewholder> onClick)
+        {
+            Data = data;
+            UpdateUIModal(data, isNew);
+            raycast.onClick.RemoveAllListeners();
+            Clicked = null;
+            Clicked += onClick;
+            raycast.onClick.AddListener(() => Clicked?.Invoke(this));
+            
+            SetSelected(false, instant: true);
+        }
+        
+        public void SetSelected(bool selected, bool instant = false)
+        {
+            IsSelected = selected;
+            selectEffect.SetActive(selected);
+
+            if (selected && !instant)
+            {
+                var t = transform;
+                t.DOKill();
+                t.DOPunchScale(t.localScale * 0.02f, 0.3f, vibrato: 12, elasticity: 1.2f).SetUpdate(true);
+            }
+        }
 
         public void UpdateUIModal(BaseSkillDataSo data, bool isNew)
         {
@@ -46,6 +76,12 @@ namespace UI.IngameViewholder
             seq.AppendInterval(0.8f).SetUpdate(true)
                 .Append(cg.DOFade(1f, 0.3f).SetUpdate(true))
                 .Join(rt.DOAnchorPosX(0f, 0.3f).SetEase(Ease.OutCubic).SetUpdate(true));
+        }
+        
+        private void OnDisable()
+        {
+            transform.DOKill();
+            newSkillText.DOKill();
         }
     }
 }
