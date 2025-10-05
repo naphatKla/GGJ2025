37917f122f00bef669bb4261164ad2360df737ff
auttasit.umpo <auttasit.umpo@gmail.com>
2025-10-05 13:59:00 +0700

    Fix Card

--- Assets/Scripts/Characters/UIDisplay/PlayerDisplay.cs
+++ Assets/Scripts/Characters/UIDisplay/PlayerDisplay.cs
@@ -524,14 +524,14 @@ namespace Characters.UIDisplay
 
             bool isNew = !skillSystem.ContainsSkillWithSameRoot(skill);
             vh.UpdateUIModal(skill, isNew);
-            vh.Bind(skill, isNew, HandleCardClicked, HandleCardHoldClicked);
             await SkillCardFeedback(skillcard.transform);
+            vh.Bind(skill, isNew, HandleCardClicked, HandleCardHoldClicked);
         }
 
         private void PanelCardFeedback(Transform tf)
         {
             tf.localPosition = new Vector2(1920, 0);
-            tf.DOKill();
+            tf.DOKill(true);
             var seq = DOTween.Sequence();
             seq.Append(tf.DOLocalMove(new Vector3(0, 0, 0), 0.7f).SetEase(Ease.OutBack))
                 .Join(tf.DOShakeRotation(0.7f, 0f, vibrato: 10, randomness: 90).SetEase(Ease.OutBack))
@@ -543,7 +543,7 @@ namespace Characters.UIDisplay
 
         private async UniTask SkillCardFeedback(Transform tf)
         {
-            tf.DOKill();
+            tf.DOKill(true);
             await tf.DOLocalRotate(new Vector3(0, 720f, 0), 0.7f, RotateMode.FastBeyond360)
                 .SetEase(Ease.OutCubic)
                 .SetUpdate(true)
@@ -554,11 +554,11 @@ namespace Characters.UIDisplay
         {
             if (vh == null) return;
             if (_currentSelectVH == vh) return;
-            if (_currentSelectVH != null) _currentSelectVH.SetSelected(false);
+            if (_currentSelectVH != null) _currentSelectVH.SetSelected(false, true);
             
             _currentSelectVH = vh;
             _currentSelect = vh.Data;
-            _currentSelectVH.SetSelected(true);
+            _currentSelectVH.SetSelected(true, true);
             
             UpdateConfirmButtonState();
         }
@@ -566,13 +566,11 @@ namespace Characters.UIDisplay
         private void HandleCardHoldClicked(SolfUpgradeViewholder vh)
         {
             if (vh == null) return;
-            if (_currentSelectVH == vh) return;
-            if (_currentSelectVH != null) _currentSelectVH.SetSelected(false);
+            if (_currentSelectVH != null) _currentSelectVH.SetSelected(false, true);
             
             _currentSelectVH = vh;
             _currentSelect = vh.Data;
-            _currentSelectVH.SetSelected(true);
-
+            _currentSelectVH.SetSelected(true, false);
             OnChooseSkillAsync().Forget();
         }
         
