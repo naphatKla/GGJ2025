c912fc1773d30c83ae56c4772e357ac5857b1132
Kla <naphat.watt@bumail.net>
2025-10-15 21:30:08 +0700

    Save

--- Assets/Scripts/Manager/SoundManager/BGMController.cs
+++ Assets/Scripts/Manager/SoundManager/BGMController.cs
@@ -18,7 +18,7 @@ namespace Manager.SoundManager
             await UniTask.WaitForSeconds(1f, true);
             SoundManager.Instance.PlayBGM(SoundName.BGM.GamePlayPhase1, fadeIn: 2f);
             await UniTask.WaitUntil(() => GameStateController.Instance.CurrentState is StartState, cancellationToken: destroyCancellationToken);
-            await UniTask.WaitUntil(() => GameTimer.Instance.GlobalTimer < 450,  cancellationToken: destroyCancellationToken);
+            await UniTask.WaitUntil(() => GameTimer.Instance.GlobalTimer < 300,  cancellationToken: destroyCancellationToken);
             SoundManager.Instance.PlayBGM(SoundName.BGM.GamePlayPhase2, fadeOut:2.5f, fadeIn:2.5f);
         }
     }
