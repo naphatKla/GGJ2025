bbd0a935a1906fdfb3bbde1925be6255da6518c5
Kla <naphat.watt@bumail.net>
2025-11-04 15:32:58 +0700

    Space mine almost done

--- Assets/Scripts/Characters/SkillSystems/SkillObjects/SpaceMineSkillObject.cs
+++ Assets/Scripts/Characters/SkillSystems/SkillObjects/SpaceMineSkillObject.cs
@@ -1,9 +1,30 @@
+using Cysharp.Threading.Tasks;
+using TMPro;
 using UnityEngine;
 
 namespace Characters.SkillSystems.SkillObjects
 {
     public class SpaceMineSkillObject : BaseSkillObject
     {
-   
+        [SerializeField] private TextMeshPro countDownText;
+        [SerializeField] private float countDownDuration = 5f;
+        private bool isTimer;
+        
+        public async UniTask WaitPlaceBombAsync()
+        {
+            if (isTimer) return;
+
+            isTimer = true;
+            float countDownTimer = countDownDuration;
+            
+            while (countDownTimer <= 0)
+            {
+                countDownTimer -= Time.deltaTime;
+                countDownText.text = countDownTimer.ToString("F1");
+                await UniTask.Yield(PlayerLoopTiming.Update);
+            }
+
+            isTimer = false;    
+        }
     }
 }
--- Assets/Scripts/Characters/SkillSystems/SkillRuntimes/SkillSpaceMineRuntime.cs
+++ Assets/Scripts/Characters/SkillSystems/SkillRuntimes/SkillSpaceMineRuntime.cs
@@ -1,14 +1,23 @@
 using System.Threading;
+using Characters.Controllers;
+using Characters.SkillSystems.SkillObjects;
 using Characters.SO.SkillDataSo;
 using Cysharp.Threading.Tasks;
+using Manager;
 
 namespace Characters.SkillSystems.SkillRuntimes
 {
     public class SkillSpaceMineRuntime : BaseSkillRuntime<SkillSpaceMineDataSo>
     {
+        public override void AssignSkillData(BaseSkillDataSo skillData, BaseController owner)
+        {
+            base.AssignSkillData(skillData, owner);
+            PoolingManager.Instance.Create<SpaceMineSkillObject>(this.skillData.SpaceMineSkillObject.name, PoolingGroupName.SkillObject, CreatePoolInstance);
+        }
+
         protected override void OnSkillStart()
         {
-            
+            PlaceBomb().Forget();
         }
 
         protected override UniTask OnSkillUpdate(CancellationToken cancelToken)
@@ -19,6 +28,25 @@ namespace Characters.SkillSystems.SkillRuntimes
         protected override void OnSkillExit()
         {
        
+        }
+
+        private SpaceMineSkillObject CreatePoolInstance()
+        {
+            SpaceMineSkillObject skillObj = Instantiate(skillData.SpaceMineSkillObject);
+            skillObj.gameObject.SetActive(false);
+            skillObj.transform.position = owner.transform.position;
+            return skillObj;
+        }
+
+        private async UniTask PlaceBomb()
+        {
+            var bomb = PoolingManager.Instance.Get<SpaceMineSkillObject>(skillData.SpaceMineSkillObject.name);
+            bomb.transform.position = owner.transform.position;
+            bomb.gameObject.SetActive(true);
+            await bomb.WaitPlaceBombAsync();
+            bomb.DamageOnTouch.EnableDamage(owner.gameObject, this, 1, skillData.BaseDamage, skillData.DamageMultiplier);
+            await UniTask.Yield();
+            PoolingManager.Current?.Release(skillData.SpaceMineSkillObject.name, bomb);
         }
     }
 }
