f2502481287c6d0b6abd37a79dd606313c790eb5
Kla <naphat.watt@bumail.net>
2025-11-04 14:01:30 +0700

    SpaceMine Setup

--- Assets/Scripts/Characters/SO/SkillDataSo/SkillSpaceMineDataSo.cs
+++ Assets/Scripts/Characters/SO/SkillDataSo/SkillSpaceMineDataSo.cs
@@ -1,9 +1,21 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Characters.SO.SkillDataSo
 {
-    [CreateAssetMenu(fileName = "SkillHarmonyOfLightData", menuName = "GameData/SkillData/SkillHarmonyOfLightData")]
+    [CreateAssetMenu(fileName = "SkillSpaceMineData", menuName = "GameData/SkillData/SkillSpaceMineData")]
     public class SkillSpaceMineDataSo : BaseSkillDataSo
     {
+        [FoldoutGroup("Damage Configs")]
+        [SerializeField] private float baseDamage;
+        
+        [Unit(Units.Percent)] [FoldoutGroup("Damage Configs")]
+        [SerializeField] private float damageMultiplier = 100;
+        
+        [FoldoutGroup("Skill Configs")] 
+        [SerializeField] private float chargeDuration;
+        
+        [FoldoutGroup("Skill Configs")]
+        [SerializeField] private float explosionRadius;
     }
 }
--- Assets/Scripts/Characters/SkillSystems/SkillRuntimes/SkillSpaceMine.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Characters.SkillSystems.SkillRuntimes
-{
-    public class SkillSpaceMine : MonoBehaviour
-    {
-        
-    }
-}
