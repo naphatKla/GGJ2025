2f21950541c7c681a7d45ec1693120d0e3eb8772
Kla <naphat.watt@bumail.net>
2025-11-07 13:54:04 +0700

    Global cooldown

--- Assets/Scripts/Characters/SkillSystems/SkillRuntimes/BaseSkillRuntime.cs
+++ Assets/Scripts/Characters/SkillSystems/SkillRuntimes/BaseSkillRuntime.cs
@@ -23,6 +23,7 @@ namespace Characters.SkillSystems.SkillRuntimes
     {
         protected float cooldown;
         protected float currentCooldown;
+        protected float globalCooldownCounter;
         protected int maxStack;
         protected int currentStack;
         public float Cooldown => cooldown;
@@ -52,8 +53,11 @@ namespace Characters.SkillSystems.SkillRuntimes
 
         public virtual void UpdateCoolDown(float deltaTime)
         {
-            if (currentCooldown <= 0) return;
-            SetCurrentCooldown(currentCooldown - deltaTime);
+            if (globalCooldownCounter > 0)
+                globalCooldownCounter -= deltaTime;
+            
+            if (currentCooldown > 0)
+                SetCurrentCooldown(currentCooldown - deltaTime);
         }
 
         public void RegisterCooldownReadyCallback(Action callback)
@@ -91,7 +95,9 @@ namespace Characters.SkillSystems.SkillRuntimes
         public override async void PerformSkill()
         {
             if (IsCooldown || IsPerforming) return;
+            if (globalCooldownCounter > 0) return;
 
+            globalCooldownCounter = skillData.GlobalCooldown;
             currentStack = Mathf.Clamp(currentStack - 1, 0, maxStack);
             
             if (currentStack <= 0)
