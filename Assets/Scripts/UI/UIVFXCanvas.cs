36d285d076d840e61bcba13379e9752db63b896a
Jittiphat Pattaradirek <jittiphat.patt@bumail.net>
2025-12-16 20:39:46 +0700

    VFX Canvas

---
+++ Assets/Scripts/UI/UIVFXCanvas.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIVFXCanvas : MonoBehaviour
+{
+    [SerializeField] private Transform gradingTransform;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
