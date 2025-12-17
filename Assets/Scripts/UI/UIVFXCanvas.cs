4523e931841fc69b8e0ed2e693f3252b8636bb2c
Jittiphat Pattaradirek <jittiphat.patt@bumail.net>
2025-12-17 15:23:35 +0700

    Grade Flame , VFX Canvas

--- Assets/Scripts/UI/UIVFXCanvas.cs
+++ Assets/Scripts/UI/UIVFXCanvas.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using ProjectExtensions;
 using UnityEngine;
 
-public class UIVFXCanvas : MonoBehaviour
+public class UIVFXCanvas : NonAutoCreateSingleton<UIVFXCanvas>
 {
-    [SerializeField] private Transform gradingTransform;
+    [SerializeField] private RectTransform gradeMainTransform;
+    [SerializeField] private RectTransform gradeVFXHolder;
+    [SerializeField] private Vector3 gradeVFXHolderOffset;
     void Start()
     {
         
@@ -13,6 +16,16 @@ public class UIVFXCanvas : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        gradeVFXHolder.transform.localScale = gradeMainTransform.transform.localScale;
+        gradeVFXHolder.transform.position = gradeMainTransform.transform.position ;
+    }
+
+    public void GradeVFXUpdate(GameObject GradeVFXprefab)
+    {
+        for (int i = gradeVFXHolder.childCount - 1; i >= 0; i--)
+        {
+            Destroy(gradeVFXHolder.GetChild(i).gameObject);
+        }
+        Instantiate(GradeVFXprefab, gradeVFXHolder);
     }
 }
