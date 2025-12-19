dc2f5e59d0a3a8475bb55e46d30bb6fa8ffdca6f
Jittiphat Pattaradirek <jittiphat.patt@bumail.net>
2025-12-19 15:16:13 +0700

    Skill Transform

--- Assets/Scripts/UI/UIVFXCanvas.cs
+++ Assets/Scripts/UI/UIVFXCanvas.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 
 public class UIVFXCanvas : NonAutoCreateSingleton<UIVFXCanvas>
 {
+    [Header("Rank UI")]
     [SerializeField] private RectTransform gradeMainTransform;
     [SerializeField] private RectTransform gradeVFXHolder;
     [SerializeField] private Vector3 gradeVFXHolderOffset;
 
+    [Header("Soft Skill UI")]
     [SerializeField] private GameObject levelUpUI;
+
+    [Header("Skill Bar UI")]
+    [SerializeField] private RectTransform primarySkillHolder;
+    [SerializeField] private RectTransform secondarySkillHolder;
+    [SerializeField] private RectTransform skillsPanel;
+    [SerializeField] private RectTransform expBar;
     void Start()
     {
         
