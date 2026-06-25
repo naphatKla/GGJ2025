e90a37d8d8e9ce6ae9c3fb9bf02468aa2c4f37eb
Kla <naphat.watt@bumail.net>
2026-06-25 16:03:18 +0700

    Save

--- Assets/Scripts/Characters/SkillSystems/SkillRuntimes/SkillNovaCutter.cs
+++ Assets/Scripts/Characters/SkillSystems/SkillRuntimes/SkillNovaCutter.cs
@@ -3,6 +3,7 @@ using Characters.Controllers;
 using Characters.SkillSystems.SkillObjects;
 using Characters.SO.SkillDataSo;
 using Cysharp.Threading.Tasks;
+using UnityEngine;
 
 namespace Characters.SkillSystems.SkillRuntimes
 {
@@ -14,12 +15,13 @@ namespace Characters.SkillSystems.SkillRuntimes
         {
             base.AssignSkillData(skillData, owner);
            _cutterObj = Instantiate(base.skillData.CutterObj, owner.Body.transform);
+           _cutterObj.transform.localPosition += (Vector3)base.skillData.CutterOffset;
            _cutterObj.gameObject.SetActive(false);
         }
 
         protected override void OnSkillStart()
         {
-            _cutterObj.gameObject.SetActive(false);
+            _cutterObj.gameObject.SetActive(true);
             _cutterObj.DamageOnTouch.EnableDamage(owner.gameObject,owner.CharacterData.CharacterId ,this, skillData.DamageHitPerSec, skillData.BaseDamagePerHit, skillData.DamageMultiplier);
         }
 
